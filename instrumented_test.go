c64c5723e73fc72418f3fd97e17efb76ca517e3a
Vincent Wong <vincent.wong@calculi.com>
2022-06-15 10:04:26 -0700

    skip instrument run in unit test

--- instrumented_test.go
+++ instrumented_test.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
-func init() {
-}
+func init() {}
 
 func TestMain(t *testing.T) {
+
+	if len(os.Getenv("TEST_COVER")) > 0 {
+		return
+	}
 
 	main()
 }
