081db38cb5e131b2e21b4b3d17aaa6a9b7ec4235
Vincent Wong <vincent.wong@calculi.com>
2022-06-15 16:07:25 -0700

    udpate entry point

--- ci/bin/init-db.sh
+++ ci/bin/init-db.sh
@@ -2,5 +2,5 @@
 
 echo "Initializing DB"
 
-mysql -u "${DB_USER}" -p"${DB_PASSWORD}" -h "${DB_HOST}" < "initdb.sql" && echo "Done initializing DB" || (echo "Failed initializing DB" && exit 1)
+mysql -u "${DB_USER}" -p"${DB_PASSWORD}" -h "${DB_HOST}" < "${APPLICATION_JOB_DIR}/bin/initdb.sql" && echo "Done initializing DB" || (echo "Failed initializing DB" && exit 1)
 
