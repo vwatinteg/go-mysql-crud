215c7a0690baf39f941bd77caf06af873f057129
Pinaki Sarkar <pinaki.sarkar@calculi.com>
2022-06-15 20:30:56 -0700

    Updated init-db.sh to call the binary as well

--- ci/bin/init-db.sh
+++ ci/bin/init-db.sh
@@ -4,3 +4,5 @@ echo "Initializing DB"
 
 mysql -u "${DB_USER}" -p"${DB_PASSWORD}" -h "${DB_HOST}" < "${APPLICATION_JOB_DIR}/bin/initdb.sql" && echo "Done initializing DB" || (echo "Failed initializing DB" && exit 1)
 
+/bin/go-mysql-crud
+
