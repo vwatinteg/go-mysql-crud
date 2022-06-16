191471080c09abc94a82513615d5a539efbd4bfe
Vincent Wong <vincent.wong@calculi.com>
2022-06-15 20:50:16 -0700

    debug entrypoint

--- ci/bin/init-db.sh
+++ ci/bin/init-db.sh
@@ -2,6 +2,12 @@
 
 echo "Initializing DB"
 
+echo "============== ENV ==============="
+env
+
+echo "============== mysql ==============="
+which mysql
+
 mysql -u "${DB_USER}" -p"${DB_PASSWORD}" -h "${DB_HOST}" < "${APPLICATION_JOB_DIR}/bin/initdb.sql" && echo "Done initializing DB" || (echo "Failed initializing DB" && exit 1)
 
 /bin/go-mysql-crud
